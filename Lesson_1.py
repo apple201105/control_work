5edd9cb3255c397163b307835ef9eb390ba7d153
apple201105 <rodiontimosenko92905@gmail.com>
2024-11-27 16:17:10 +0700

    Add files via upload

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,9 +1,5 @@
-import math
 
-
-def determining_the_time(h):
-    res = 2 * h / 9.8
-    return math.sqrt(res)
-
-h = int(input('Введите высоту:'))
-print(determining_the_time(h))
+a = int(input('Введите 1 число:'))
+b = int(input('Введите 2 число:'))
+c = int(input('Введите 3 число:'))
+print((a+b+c)/3)
