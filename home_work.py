bff35c5844652b365b5ff14e5581dda8506a1162
apple201105 <rodiontimosenko92905@gmail.com>
2024-12-15 15:02:33 +0700

    Add files via upload

--- home_work.py
+++ home_work.py
@@ -2,16 +2,24 @@ import pandas as pd
 from openpyxl import load_workbook
 wb = load_workbook('список.xlsx')
 ws = wb.active
-
+Iv = (5+5+2+3+4)/5
+Pet = (5+5+2+3+4)/5
+Ar = (5+5+2+3+4)/5
+Rod = (5+5+5+5+5)/5
+And = (5+5+2+3+4)/5
+Vas = (5+5+2+3+4)/5
+jp = (5+5+2+3+4)/5
+Kir = (5+5+2+3+4)/5
 ws['G1'] = "arithmetic mean"
-ws['G2'] = '=СРЗНАЧ(B2:F2)'
-ws['G3'] = '=СРЗНАЧ(B3:F3)'
-ws['G4'] = '=СРЗНАЧ(B4:F4)'
-ws['G5'] = '=СРЗНАЧ(B5:F5)'
-ws['G6'] = '=СРЗНАЧ(B6:F6)'
-ws['G7'] = '=СРЗНАЧ(B7:F7)'
-ws['G8'] = '=СРЗНАЧ(B8:F8)'
-ws['G9'] = '=СРЗНАЧ(B9:F9)'
+ws['G2'] = Iv
+ws['G3'] = Pet
+ws['G4'] = Ar
+ws['G5'] = Rod
+ws['G6'] = And
+ws['G7'] = Vas
+ws['G8'] = jp
+ws['G9'] = Kir
+
 
 wb.save('список.xlsx')
 df_excel = pd.read_excel('список.xlsx')
