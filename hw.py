9593aecc3fe66cbefb3cc329f70f52a2f51e4e30
apple201105 <rodiontimosenko92905@gmail.com>
2024-12-16 20:56:13 +0700

    Update hw.py

--- hw.py
+++ hw.py
@@ -4,19 +4,15 @@ wb = load_workbook('список.xlsx')
 ws = wb.active
 row = ws.max_row
 ws['G1'] = "AM"
-ws['H1'] = "Average"
 for i in range(2, row+1):
-    AM    = "G"
-    AVG   = "H"
-    AM    = AM    + str(i)
-    AVG   = AVG   + str(i)
+    AM = "G"
+    AM  = AM  + str(i)
     sum_row = 0
     col_list = ["B", "C", "D", "E", "F"]
     for j in range(len(col_list)):
         col_list[j] = col_list[j] + str(i)
         sum_row = sum_row + ws[col_list[j]].value
     ws[AM] = sum_row/len(col_list)
-    ws[AVG] = "= AVERAGE(" + col_list[0]+":"+col_list[4]+")"
 wb.save('список.xlsx')
 wb.close()
 df_excel = pd.read_excel('список.xlsx')
