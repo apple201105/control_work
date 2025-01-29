8d8c52d87318a186d8d7923705103b360c221e22
apple201105 <rodiontimosenko92905@gmail.com>
2025-01-29 16:32:53 +0700

    Update work with pandas(2).py

--- work with pandas(2).py
+++ work with pandas(2).py
@@ -14,7 +14,7 @@ while True:
         if row_ind < 0 or row_ind>= df_excel.shape[0] or col_ind < 0 or col_ind >= df_excel.shape[1]:
             print(" Индекс вне диапазона.")
             continue
-        act = input("Введите 'внести' для ввода значения или 'очистить' для очистки ячейки: ").strip().lower()
+        act = input("Введите 'внести' для ввода значения или 'очистить' для очистки ячейки или подсчитать среднее значение или 'мин и макс': ").strip().lower()
         if act == 'внести':
             val = input("Введите значение для обновления: ")
             df_excel.iat[row_ind, col_ind] = val
@@ -22,10 +22,36 @@ while True:
         elif act == 'очистить':
             df_excel.iat[row_ind, col_ind] = None
             print("Значение очищено.")
-        else:
-            print(" Пожалуйста, введите 'внести' или 'очистить'.")
+        elif act == 'среднее значение':
+            choise = str(input('Введите "колонка" или "столбец":'))
+            if choise == "колонка":
+                col_name = str(input('Введите название калонки'))
+                print(df_excel.iloc[col_ind].mean())
+            elif choise == "столбец":
+                row_name = str(input('Введите название строки'))
+                print(df_excel.iloc[row_ind].mean())
+        elif act == 'мин и макс':
+            column_name = str(input('введите название колонки'))
+            row_index = int(input('введите индекс строки'))
+
+            if column_name in df_excel.columns:
+                min_value = df_excel[column_name].min()
+                max_value = df_excel[column_name].max()
+                print(f'Минимальное значение в колонке "{column_name}": {min_value}')
+                print(f'Максимальное значение в колонке "{column_name}": {max_value}')
+            else:
+                print(f'Колонка "{column_name}" не найдена в таблице.')
+
+            if row_index < len(df_excel):
+                min_value_row = df_excel.iloc[row_index].min()
+                max_value_row = df_excel.iloc[row_index].max()
+                print(f'Минимальное значение в строке {row_index}: {min_value_row}')
+                print(f'Максимальное значение в строке {row_index}: {max_value_row}')
+            else:
+                print(f'Строка с индексом {row_index} не найдена в таблице.')
+    
     except ValueError:
         print("Ошибка: Введите корректные значения.")
     except Exception as e:
         print(f"Произошла ошибка: {e}")
-print("Программа завершена.")
+    print("Программа завершена.")
