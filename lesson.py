4bcc1138ce458c58b3e73d7f760d80196cbe7f4a
apple201105 <rodiontimosenko92905@gmail.com>
2025-02-09 09:29:40 +0700

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -9,3 +9,19 @@ else:
     df_excel.to_excel(output_file_path, index=False)
 
     print(f"Результаты сохранены в файл: {output_file_path}")
+
+
+
+# Код для нахождения дисперсии по строкам DataFrame
+
+import pandas as pd
+
+
+def calculate_variance_by_row(df):
+    numeric_columns = df.select_dtypes(include=['number'])
+    df['dISP'] = numeric_columns.var(axis=1)
+    return df
+
+df = pd.read_excel('список.xlsx')
+df = calculate_variance_by_row(df)
+print(df)
