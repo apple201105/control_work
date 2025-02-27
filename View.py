cdfeb9f84f8aebea068308790b240b1def57eea9
apple201105 <rodiontimosenko92905@gmail.com>
2025-02-27 11:14:19 +0700

    Add files via upload

---
+++ View.py
@@ -0,0 +1,18 @@
+# View (Представление) - отвечает за отображение и ввод данных
+def display_menu():
+    print("\nВыбери действие: 1 - сложение, 2 - вычитание, 3 - умножение,4 - деление, 0 - выход")
+    choice = input("Введи номер действия: ")
+    return choice
+
+
+
+def get_numbers():
+    a = float(input("Введи первое число: "))
+    b = float(input("Введи второе число: "))
+    return a, b
+
+def show_result(result):
+    print(f"Результат: {result}")
+
+def show_message(message):
+    print(message)
