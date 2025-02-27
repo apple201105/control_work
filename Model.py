37999fcaa27bc60e90cd7ffbb142426f65cb4101
apple201105 <rodiontimosenko92905@gmail.com>
2025-02-27 11:14:08 +0700

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,14 @@
+# Model (Модель) - отвечает за логику вычислений
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Ошибка: деление на ноль"
+    return a / b
